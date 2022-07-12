f86e8495d05915f88e5a3f681a48a590f8cbeff8
HonamJSH <99239284+HonamJSH@users.noreply.github.com>
2022-07-12 14:26:35 +0900

    //회원가입 @GetMapping 추가

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
@@ -19,6 +20,7 @@ public class MemberController {
     }
 
     // 회원 가입
+    @GetMapping("/members/new")
     public String createForm() {
         return "members/createMemberForm";
     }
